3e42186c3fe5d311046845ebd225a386858d273c
soy <soysauce1223@naver.com>
2024-12-30 13:33:14 +0900

    Initial Commit

---
+++ Sparta10_Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sparta10_Pokedex
+//
+//  Created by seohuibaek on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sparta10_PokedexTests/Sparta10_PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sparta10_PokedexTests.swift
+//  Sparta10_PokedexTests
+//
+//  Created by seohuibaek on 12/30/24.
+//
+
+import Testing
+@testable import Sparta10_Pokedex
+
+struct Sparta10_PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
