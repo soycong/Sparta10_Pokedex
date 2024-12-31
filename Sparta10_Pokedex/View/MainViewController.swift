ae7bcb46e6f77181cc619e8586b244654843dd96
soy <soysauce1223@naver.com>
2024-12-31 13:29:42 +0900

    [Feat] Pokemon CollectionView 생성

--- Sparta10_Pokedex/View/MainViewController.swift
+++ Sparta10_Pokedex/View/MainViewController.swift
@@ -13,6 +13,18 @@ class MainViewController: UIViewController {
     private let viewModel = MainViewModel()
     private let disposeBag = DisposeBag()
     
+    private var pokemonList = [PokemonList]()
+    
+    private lazy var collectionView: UICollectionView = {
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+//        collectionView.register(PosterCell.self, forCellWithReuseIdentifier: PosterCell.id)
+//        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeaderView.id)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .black
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //bindViewModel()
@@ -21,9 +33,12 @@ class MainViewController: UIViewController {
     private func bindViewModel() {
         // 포켓몬 리스트 데이터 구독
         viewModel.pokemonSubject
+            .observe(on: MainScheduler.instance) //MainThread 에서 동작해라!
             .subscribe(onNext: { pokemonList in
                 print("받은 포켓몬 리스트:", pokemonList)
-                // 여기서 TableView나 CollectionView 업데이트
+
+                self.pokemonList = pokemonList
+                self.collectionView.reloadData()
             }, onError: { error in
                 print("에러 발생:", error)
             })
@@ -40,5 +55,21 @@ class MainViewController: UIViewController {
             .disposed(by: disposeBag)
     }
 
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemonList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PokemonCollectionViewCell.id, for: indexPath) as? PokemonCollectionViewCell  else { return UICollectionViewCell() }
+
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    
 }
 
