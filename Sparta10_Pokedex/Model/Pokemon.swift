85134300a3d95d08b50cb249c69d79cce7f301a8
soy <soysauce1223@naver.com>
2024-12-31 11:47:46 +0900

    [Feat] Pokemon Model 생성

---
+++ Sparta10_Pokedex/Model/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  Sparta10_Pokedex
+//
+//  Created by seohuibaek on 12/30/24.
+//
+
+import Foundation
+
+struct PokemonListResponse: Codable {
+    let results: [PokemonList]
+}
+
+struct PokemonList: Codable {
+    let name: String?
+    let url: String?
+}
+
+struct PokemonDetailResponse: Codable {
+    let results: [PokemonDetail]
+}
+
+struct PokemonDetail: Codable {
+    let id: Int
+    let name: String
+    let height: Int
+    let weight: Int
+    let sprites: Sprites
+}
+
+struct Sprites: Codable {
+    let front_default: String?
+}
