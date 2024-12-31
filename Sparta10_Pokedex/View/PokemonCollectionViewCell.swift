5476560ffc0b03593a2efbbe1f54ea5a55b63a29
soy <soysauce1223@naver.com>
2024-12-31 13:29:23 +0900

    [Feat] Pokemon collectionViewCell 생성

---
+++ Sparta10_Pokedex/View/PokemonCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Sparta10_Pokedex
+//
+//  Created by seohuibaek on 12/31/24.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    static let id: String = "PokemonCollectionViewCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .darkGray
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // cell 버벅임 현상 해결
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    func configure(with pokemonDetail:PokemonDetail) {
+//        guard let posterPath = movie.posterPath else { return }
+//        let urlString = "https://image.tmdb.org/t/p/w500/\(posterPath)"
+//        guard let url = URL(string: urlString) else { return }
+//        
+//        DispatchQueue.global().async { [weak self] in
+//            if let data = try? Data(contentsOf: url) {
+//                if let image = UIImage(data: data) {
+//                    DispatchQueue.main.async {
+//                        self?.imageView.image = image
+//                    }
+//                }
+//            }
+//        }
+    }
+}
